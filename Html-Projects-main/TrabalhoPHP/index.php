6a4e967b0b572e1f0df1d21160dd680bdf934c54
Walyson Henrique Ozório Sobrinho <20231STADS005@cefetes.br>
2024-12-16 10:08:55 -0300

    Carousel

--- Html-Projects-main/TrabalhoPHP/index.php
+++ Html-Projects-main/TrabalhoPHP/index.php
@@ -18,7 +18,8 @@
 
     <header>
         <div class="spacenav">
-
+        <h6>CONTE COM QUEM MAIS AMA E ENTENDE DE CARROS
+        </h6>
         </div>
         <span></span>
         <section class="menu">
@@ -59,7 +60,66 @@
     </header>
 
     <main>
+        <div>
+        <div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7" aria-label="Slide 8"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8" aria-label="Slide 9"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="9" aria-label="Slide 10"></button>
 
+
+
+
+</div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img\4f94fa21-dda8-4762-b768-08b6b48b0c0d-banner-novo-208-desktop.avif" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img\8eead4c7-8eba-4e0c-8388-8750c8bb3621-deskbasalt.avif" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img\62f9c25c-f126-4b46-be6b-04c7ef8c66f5-banner-pcd-peugeot.avif" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img\245d4721-46c1-4980-ba06-5ec288230cd2-cretabanner.avif" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img\3516bdd6-3521-41e9-8ae3-a82dfa9f7a45-pasalide-1.avif" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img\174059a6-017f-4781-8106-ff1384f11e2d-banner-f-150.avif" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img\bc70b3a3-324a-494f-952e-1290be2a1a95-3.avif" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img\c0c435f0-0cbd-4d38-8136-9c2a48d8641e-rampage.avif" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img\c041b679-ade0-4c73-8934-e9a0b0956041-ranger-xls.avif" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img\e37e46e3-393f-44f2-a130-0c991870030f-202410_kia_carnival_banner.avif" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+        </div>
     </main>
 
     <footer></footer>
